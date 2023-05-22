29e234623356ed87a7383706754f23a17d6ad2ac
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-22 21:37:03 +0330

    create the content models use abstract model

--- courses/models.py
+++ courses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey,GenericRelation
 
 
 class Subject(models.Model):
@@ -43,6 +43,37 @@ class Module(models.Model):
 
 class Content(models.Model):
 	module=models.ForeignKey(Module,on_delete=models.CASCADE,related_name='contents')
-	content_type=models.ForeignKey(ContentType,on_delete=models.CASCADE)
+	content_type=models.ForeignKey(ContentType,on_delete=models.CASCADE,limit_choices_to={'model__in':(
+'text',
+'video',
+'image',
+'file')})
 	object_id=models.PositiveIntegerField()
 	content_object=GenericForeignKey('content_type','object_id')
+
+
+class BaseItem(models.Model):
+	owner=models.ForeignKey(User,on_delete=models.CASCADE,related_name='%(class)s_related')
+	title=models.CharField(max_length=100)
+	created=models.DateTimeField(auto_now_add=True)
+	updated=models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract=True
+
+	def __str__(self):
+		return self.title
+
+
+class Text(BaseItem):
+	content=models.TextField()
+
+
+class File(BaseItem):
+	file=models.FileField(upload_to='files')
+
+class  Image(BaseItem):
+	file=models.FileField(upload_to='images')
+
+class Video(BaseItem):
+	url=models.URLField()
