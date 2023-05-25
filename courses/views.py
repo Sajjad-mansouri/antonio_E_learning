1f7e098273eb3778a7c5f1b1f90ec55d8d68b2c3
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-26 01:28:44 +0330

    using mixin for cbv

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Course
 
-class CourseListView(ListView):
-	model=Course
-	template_name='courses/course/list.html'
+
+
+class OwnerMixin:
 	def get_queryset(self):
 		query=super().get_queryset()
 		return query.filter(owner=self.request.user)
+
+class OwnerEditMixin:
+	def form_valid(self,form):
+		form.instanc.owner=self.request.user
+		return super().form_valid(form)
+
+class OwnerCourseMixin(OwnerMixin):
+	model=Course
+	field=['title','subject','slug','overview']
+	success_url=reverse_lazy('course-list')
+
+class OwnerCourseEditMix(OwnerCourseMixin,OwnerEditMixin):
+	template_name='courses/course/course-form.html'
+
+
+
+
+class CourseListView(OwnerCourseMixin,ListView):
+	template_name='courses/course/list.html'
+
+class CourseCreateView(OwnerCourseEditMix,CreateView):
+	pass
+class CourseUpdateView(OwnerCourseEditMix,UpdateView):
+	pass
+
+class CourseDeleteView(OwnerCourseMixin,DeleteView):
+		template_name='courses/course/course-delete.html'
+
