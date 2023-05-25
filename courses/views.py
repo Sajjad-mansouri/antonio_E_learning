b5342d85b45b974a03f71783482c3d26bc703fdb
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-26 00:13:04 +0330

    create clv for content management system

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Course
 
-# Create your views here.
+class CourseListView(ListView):
+	model=Course
+	template_name='courses/course/list.html'
+	def get_queryset(self):
+		query=super().get_queryset()
+		return query.filter(owner=self.request.user)
