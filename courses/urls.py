591c7a5d158095bf558cd5f7ddba0ef5687cd090
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-26 21:06:31 +0330

    working with groups and permissions:restricting access to cbv

---
+++ courses/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+	path('',views.CourseListView.as_view(),name='course-list'),
+	path('create/',views.CourseCreateView.as_view(),name='course-create'),
+	path('<pk>/update/',views.CourseUpdateView.as_view(),name='course-update'),
+	path('<pk>/delete/',views.CourseDeleteView.as_view(),name='course-delete'),
+
+
+
+]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.urls import reverse_lazy
 from .models import Course
 
@@ -13,12 +14,12 @@ class OwnerMixin:
 
 class OwnerEditMixin:
 	def form_valid(self,form):
-		form.instanc.owner=self.request.user
+		form.instance.owner=self.request.user
 		return super().form_valid(form)
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin,LoginRequiredMixin,PermissionRequiredMixin):
 	model=Course
-	field=['title','subject','slug','overview']
+	fields=['title','subject','slug','overview']
 	success_url=reverse_lazy('course-list')
 
 class OwnerCourseEditMix(OwnerCourseMixin,OwnerEditMixin):
@@ -28,13 +29,18 @@ class OwnerCourseEditMix(OwnerCourseMixin,OwnerEditMixin):
 
 
 class CourseListView(OwnerCourseMixin,ListView):
-	template_name='courses/course/list.html'
+	template_name='courses/course/course-list.html'
+	permission_required='courses.view_course'
 
 class CourseCreateView(OwnerCourseEditMix,CreateView):
-	pass
+		permission_required='courses.add_course'
+
 class CourseUpdateView(OwnerCourseEditMix,UpdateView):
-	pass
+		permission_required='courses.change_course'
+
 
 class CourseDeleteView(OwnerCourseMixin,DeleteView):
 		template_name='courses/course/course-delete.html'
+		permission_required='courses.delete_course'
+
 
