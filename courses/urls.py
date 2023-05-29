a176984985f55f4b49ed4491e1d61fdf3ef06402
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-29 23:37:53 +0330

    add create and update content url patterns

--- courses/urls.py
+++ courses/urls.py
@@ -6,7 +6,10 @@ urlpatterns=[
 	path('create/',views.CourseCreateView.as_view(),name='course-create'),
 	path('<pk>/update/',views.CourseUpdateView.as_view(),name='course-update'),
 	path('<pk>/delete/',views.CourseDeleteView.as_view(),name='course-delete'),
-	path('<pk>/module/',views.CourseModule.as_view(),name='course-module')
+	path('<pk>/module/',views.CourseModule.as_view(),name='course-module'),
+	path('module/<int:module_id>/content/<model_name>/',views.CourseContent.as_view(),name='content-create'),
+	path('module/<int:module_id>/content/<model_name>/<id>/',views.CourseContent.as_view(),name='content-update')
+
 
 
 
