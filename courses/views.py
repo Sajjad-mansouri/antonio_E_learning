ba586a132ef035fba497e5dae2a440681ff2d9ad
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-05-29 23:31:49 +0330

    adding content to course modules, create class with TemplateResponseMixin and View

--- courses/views.py
+++ courses/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.views.generic.base import TemplateResponseMixin
+from django.views import View
+from django.forms import modelformset_factory
 from django.shortcuts import get_object_or_404
+from django.apps import app
 from django.urls import reverse_lazy,reverse
 from .forms import Modul_Formset
-from .models import Course
+from .models import Course,Content
 
 
 
@@ -73,3 +77,42 @@ class CourseModule(UpdateView):
 
 	def get_success_url(self):
 		return reverse('course-list')
+
+
+class CourseContent(TemplateResponseMixin,View):
+	template_name='courses/content/content.html'
+
+	def get_model(self,model_name):
+		if model_name in ['text', 'video', 'image', 'file']:
+			return apps.get_model(app_label='courses',model_name=model_name)
+
+		else:
+			return None
+	def dispatch(self,request,module_id,model_name,id=None):
+		self.module=get_object_or_404(Module,id=module_id,course__owner=request.user)
+		self.model=self.get_model(model_name)
+		if id:
+			self.obj=get_object_or_404(self.model,id=id,owner=request.user)
+		return super().dispatch(request,module_id,model_name,id)
+
+	def get_form(self,model,*args,**kwargs):
+		form=modelformset_factory(model,exclude=['owner','created','updated','order'])
+		return form(*args,**kwargs)
+
+	def get(self,request,module_id,model_name,id=None):
+		form=self.get_form(self.model,instance=self.obj)
+		return self.render_to_response({'form':form,'object':self.obj})
+
+	def post(self,request,module_id,model_name,id=None):
+		form=self.get_form(self.model,instance=self.obj,data=request.POST,files=request.FILES)
+		if form.is_valid():
+			obj=form.save(commit=False)
+			obj.owner=request.user
+			obj.save()
+			if not id:
+				Content.objects.create(module=self.module,content_object=obj)
+
+			return redirect('content-list',self.module_id)
+		else:
+			return self.render_to_response({'form':form,'object':self.obj})
+
