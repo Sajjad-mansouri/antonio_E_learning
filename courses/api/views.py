849598a9387982a9c1e234100a9ba5d4429bab72
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-06-05 17:46:56 +0330

    handling authentication

--- courses/api/views.py
+++ courses/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
 from courses.models import Subject,Course,Module
 from .serializers import SubjectSerializer
 
@@ -15,6 +16,7 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
+	authentication_classes=[BasicAuthentication]
 
 	def post(self,request,pk,format=None):
 		course=get_object_or_404(Course,pk=pk)
